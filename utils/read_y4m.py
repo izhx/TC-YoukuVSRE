856a23535c159134a72ba3022d140afa071f2f4e
izhx <izhx404@gmail.com>
2019-05-21 09:26:48 +0800

    修改read_y4m，去除PIL依赖，只用numpy。

--- utils/read_y4m.py
+++ utils/read_y4m.py
@@ -1,6 +1,6 @@
 import gc
 import numpy as np
-from PIL import Image, ImageOps
+# from PIL import Image, ImageOps
 
 
 def read_y4m(file_path):
@@ -29,12 +29,19 @@ def read_y4m(file_path):
     start_v, end_v = (n_pixel + (n_pixel >> 2), n_pixel + (n_pixel >> 1))
     frames = list()
 
+    def extend_uv(c: np.ndarray) -> np.ndarray:
+        cc = c.repeat(2)
+        cc_cc = np.vstack((cc, cc))
+        c4 = np.hsplit(cc_cc, c.shape[0])
+        return np.vstack(c4)
+
     for frame in yuv_frames:
         y = np.array((frame[:end_y]), dtype=np.uint8).reshape(height, width)
         u = np.array(frame[start_u:end_u], dtype=np.uint8).reshape(uvh, uvw)
         v = np.array(frame[start_v:end_v], dtype=np.uint8).reshape(uvh, uvw)
-        u = np.array(ImageOps.scale(Image.fromarray(u), 2, Image.BICUBIC))
-        v = np.array(ImageOps.scale(Image.fromarray(v), 2, Image.BICUBIC))
+        # u = np.array(ImageOps.scale(Image.fromarray(u), 2, Image.BICUBIC))
+        # v = np.array(ImageOps.scale(Image.fromarray(v), 2, Image.BICUBIC))
+        u, v = extend_uv(u), extend_uv(v)
         frames.append(np.array([y, u, v]).transpose((1, 2, 0)))
 
     del header, raw_frames, yuv_frames
