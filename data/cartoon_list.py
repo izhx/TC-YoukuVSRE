b1e9cb94b531775e891f35ad85cc09dc8cf6b830
Qiao_Fengyi <35530678+midebuxing@users.noreply.github.com>
2019-06-02 10:20:29 +0800

    Create cartoon_list.py

---
+++ data/cartoon_list.py
@@ -0,0 +1,11 @@
+
+CARTOONS = ['Youku_00005', 'Youku_00020', 'Youku_00030', 'Youku_00032', 'Youku_00038',
+            'Youku_00039', 'Youku_00043', 'Youku_00048', 'Youku_00055', 'Youku_00056',
+            'Youku_00058', 'Youku_00059', 'Youku_00062', 'Youku_00065', 'Youku_00073',
+            'Youku_00084', 'Youku_00086', 'Youku_00089', 'Youku_00092', 'Youku_00096',
+            'Youku_00098', 'Youku_00104', 'Youku_00105', 'Youku_00106', 'Youku_00109',
+            'Youku_00110', 'Youku_00113', 'Youku_00116', 'Youku_00120', 'Youku_00133',
+            'Youku_00134', 'Youku_00135', 'Youku_00141', 'Youku_00143', 'Youku_00144',
+            'Youku_00146', 'Youku_00148', 'Youku_00149', 'Youku_00151', 'Youku_00160',
+            'Youku_00170', 'Youku_00183', 'Youku_00185', 'Youku_00186', 'Youku_00190',
+            'Youku_00193']
